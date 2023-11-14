2400831d1614ae3a2921fef291714d2d77547127
Paul J. Han <pauljhan@utexas.edu>
2023-11-14 15:06:15 -0600

    added page fault vector to vector table

--- routines/vector_table.asm
+++ routines/vector_table.asm
@@ -1,7 +1,8 @@
 .ORIG x0200
 .FILL x0000 ;Empty
 .FILL x1200 ;ISR, vector x01
-.FILL x1600 ;Protection exception handler, vector x02
+.FILL x1400 ;Page fault handler, vector x02
 .FILL x1A00 ;Unaligned access exception handler, vector x03
-.FILL x1C00 ;Unknown opcode exception handler, vector x04
+.FILL x1600 ;Protection exception handler, vector x04
+.FILL x1C00 ;Unknown opcode exception handler, vector x05
 .END
